a46fcb8509805d9b99a4e2112c9249483cfa9187
Pablo Barrientos <pablobarrientos.cba@gmail.com>
2024-07-03 12:56:47 -0300

    just formatting

--- 7kiu/DescendingOrder.cs
+++ 7kiu/DescendingOrder.cs
@@ -5,9 +5,7 @@ Essentially, rearrange the digits to create the highest possible number.
 
 Examples:
 Input: 42145 Output: 54421
-
 Input: 145263 Output: 654321
-
 Input: 123456789 Output: 987654321
 */
 
