313412ca0dd431fc5010ed5c1f64ca14da7ab852
Pablo Barrientos <pablobarrientos.cba@gmail.com>
2024-07-04 15:40:53 -0300

    complete excercise

--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
 ﻿
 // Solution.DoReverseSentence(); // Reverse words in a sentence
 // Console.WriteLine(Solution.DoDescendingOrderBest(5434567)); // Return Descending order
-// Console.WriteLine(Kata.GetMiddle("katabest3"));
-
-Kata.MinMax([1]).ToList().ForEach(item => System.Console.WriteLine( item.ToString()));
+// Console.WriteLine(Kata.GetMiddle("katabest3")); // Get Middle Char(s)
+Kata.MinMax([1]).ToList().ForEach(item => Console.WriteLine( item.ToString()));
